bee7e3619219ae2e47f17275eaef5aedd5a9d2ec
Simon Sandlund <simon.sandlund@gmail.com>
2011-05-13 17:10:09 +0200

    Added a deftype for (unsigned-byte 32).

--- um/lisp/universal-machine.lisp
+++ um/lisp/universal-machine.lisp
@@ -3,6 +3,9 @@
 
 (in-package #:universal-machine)
 
+;; We are dealing with 32-bit unsigned integers.
+(deftype uint32 () '(unsigned-byte 32))
+
 (defmacro wrap-around (n)
    `(logand #xFFFFFFFF ,n))
 
@@ -11,7 +14,7 @@
     (when (/= 0 (mod (file-length in) 4))
       (error "The file does not contain an even number of 32-bit words."))
     ;; Read the program big endian style.
-    (let ((program (make-array (/ (file-length in) 4) :element-type '(unsigned-byte 32))))
+    (let ((program (make-array (/ (file-length in) 4) :element-type 'uint32)))
       (do ((index 0 (1+ index)))
           ((>= index (length program)))
         (let* ((b1 (read-byte in))
@@ -27,9 +30,9 @@
 
 (defun main-loop (allocated-mem regs free-indices pc)
   (declare (optimize (speed 3) (safety 0) (debug 0)))
-  (declare (fixnum pc)
-           ((simple-array (unsigned-byte 32)) regs)
-           ((simple-array (simple-array (unsigned-byte 32) 1) 1) allocated-mem))
+  (declare (uint32 pc)
+           ((simple-array uint32) regs)
+           ((simple-array (simple-array uint32 1) 1) allocated-mem))
   (macrolet ((mem (index) `(aref allocated-mem ,index))
              (reg (index) `(aref regs ,index)))
     (loop 
@@ -38,7 +41,7 @@
               (b (logand (ash instr -3) 7))
               (c (logand instr 7))
               (opcode (ash instr -28)))
-         (declare (fixnum instr))
+         (declare (uint32 instr))
          (case opcode
            ;; conditional move
            (0 (when (/= 0 (reg c))
@@ -61,7 +64,9 @@
            ;; not-and
            (6 (setf (reg a)
                     (wrap-around (lognot (logand (reg b) (reg c))))))
+	   ;; halt
            (7 (return))
+	   ;; allocation
            (8 (let ((new-array (make-array (reg c) :element-type '(unsigned-byte 32))))
                 (if (not (null free-indices))
                     (let ((index (car free-indices)))
@@ -74,13 +79,18 @@
                       (setf (reg b) index)
                       (setf free-indices (loop for i from (1+ index) to (1- (length allocated-mem))
 					    collect i))))))
+	   ;; abandon (deallocation)
            (9 (setf free-indices (cons (reg c) free-indices)))
+	   ;; output
            (10 (princ (code-char (reg c))))
+	   ;; input
            (11 (setf (reg c) (char-code (read-char))))
+	   ;; load program
            (12 (progn (when (/= 0 (reg b))
-                        (setf (mem 0) 
+                        (setf (mem 0)
                               (copy-seq (mem (reg b)))))
                       (setf pc (1- (reg c)))))
+	   ;; orthography
            (13 (let ((a (logand (ash instr -25) 7)))
                  (setf (reg a)
                        (logand instr #x1FFFFFF))))
@@ -88,7 +98,7 @@
          (incf pc)))))
 
 (defun array-extend (array)
-  "Creates a copy of ARRAY that is twice as long."
+  "Create a copy of ARRAY that is twice as long."
   (let ((new-array (make-array (* 2 (length array)))))
     (loop for x across array and i upfrom 0 
        do (setf (aref new-array i) x))
@@ -96,7 +106,7 @@
     new-array))
 
 (defun start (filename)
-  (let ((regs (make-array 8 :element-type '(unsigned-byte 32)))
+  (let ((regs (make-array 8 :element-type 'uint32))
         (allocated-mem (make-array 1000))
         (free-indices (loop for i from 1 to 999 collect i)))
     (setf (elt allocated-mem 0) (load-program filename))
